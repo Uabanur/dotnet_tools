16bfd52ccf2475bc1eb4fc031c7bdf4c9df4dabe
= <=>
2023-12-08 08:33:12 +0100

    RetryPolly setup and signalr misc

--- AsyncMessagingSignalR/Common/MessageModel.cs
+++ AsyncMessagingSignalR/Common/MessageModel.cs
@@ -4,4 +4,6 @@ public class MessageModel
 {
     public string Message { get; set; } = default!;
     public string ConnectionId { get; set; } = default!;
+    public override string ToString() 
+        => $"MessageModel {{Message='{Message}', ConnectionId='{ConnectionId}'}}";
 }
